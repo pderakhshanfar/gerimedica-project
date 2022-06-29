3ed5396d9c4b3af36ee4c683c6928d6b8df7faff
Pouria Derakhshanfar <pouria.derakhshanfar@gmail.com>
2022-06-29 21:10:34 +0200

    Add failing test for CSV upload

--- src/test/java/nl/gerimedica/IntegrationTest.java
+++ src/test/java/nl/gerimedica/IntegrationTest.java
@@ -8,10 +8,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,6 +33,7 @@ public class IntegrationTest {
 
 
     private final String serverUrl = "/api/grecord";
+    private final String userDir = System.getProperty("user.dir");
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -33,6 +47,68 @@ public class IntegrationTest {
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         // The get all request without uploading any CSV should return an empty list.
         Assertions.assertThat(jsonArray.size()).isEqualTo(0);
+    }
+
+
+    @Test
+    public void testUploadAndFullGetAll() throws IOException {
+        //events
+        // upload is a method for making a fake CSV file and then upload it
+        this.upload();
+        ResponseEntity<String> response = restTemplate.getForEntity(serverUrl, String.class);
+        JsonArray jsonArray = new Gson().fromJson(response.getBody(), JsonArray.class);
+        //assertion
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        // Since, sampleCsvGenerate adds 4 records, we check the size
+        Assertions.assertThat(jsonArray.size()).isEqualTo(4);
+    }
+
+
+    private ResponseEntity<String> upload() throws IOException {
+        Path sample_csv_file = Paths.get(userDir,"sample.csv");
+        // generate CSV file
+        sampleCsvGenerate(sample_csv_file);
+        // Prepare header
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        // Prepare body
+        MultiValueMap<String, Object> body
+                = new LinkedMultiValueMap<>();
+        FileSystemResource resource = new FileSystemResource(sample_csv_file);
+        body.add("file", resource);
+
+        // Send POST request
+        HttpEntity<MultiValueMap<String, Object>> requestEntity
+                = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+
+        // Delete file
+        File csvFile = sample_csv_file.toFile();
+        csvFile.delete();
+        // Return the response
+        return response;
+    }
+
+
+    private void sampleCsvGenerate(Path sample_csv_file) throws IOException {
+        BufferedWriter writer = Files.newBufferedWriter(sample_csv_file);
+        // These entries are taken from the exercise.csv file
+        CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT); {
+            csvPrinter.printRecord("source",
+                    "codeListCode",
+                    "code",
+                    "displayValue",
+                    "longDescription",
+                    "fromDate",
+                    "toDate",
+                    "sortingPriority");
+            csvPrinter.printRecord("ZIB","ZIB002","Type 2","Als een worst, maar klonterig","","01-01-2019","","");
+            csvPrinter.printRecord("ZIB","ZIB003","276885007","Kern temperatuur (invasief gemeten)","","01-01-2019","","");
+            csvPrinter.printRecord("ZIB","ZIB002","Type 4","Als een worst of slang, glad en zacht","","01-01-2019","","");
+            csvPrinter.printRecord("ZIB","ZIB001","271636001","Polsslag regelmatig","The long description is necessary","01-01-2019","","1");
+
+            csvPrinter.flush();
+        }
     }
 
 }
